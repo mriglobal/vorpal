20477081f91e6635f41cf5abcb20662b6c28c690
Phil Davis <pdavis@mriglobal.org>
2019-03-25 16:49:29 -0400

    added multiprocess reference mapping script along with print statements for debugging

---
+++ referencemapping_mp.py
@@ -0,0 +1,93 @@
+import os
+import pandas as pd
+import numpy as np
+import time
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+from Bio.SeqUtils import nt_search
+from multiprocessing import Pool
+import pickle
+from skbio import DNA
+import argparse
+
+parser = argparse.ArgumentParser(description="Mapping Degenerate Kmers to Reference Sequences")
+parser.add_argument('-r',required=True, help="Concatenated References Fasta")
+parser.add_argument('-k',required=True, help='Degenerate Kmers Fasta')
+
+myargs=parser.parse_args(['-r','Flu_A_seg3_test.fasta','-k','NC_0073710_17mers_sparse3.0_degenerate_primers.fasta'])
+os.chdir('/home/pdavis/Documents/RNAproject/Influenza_A/segment_3')
+def pscore(primer,primerset):
+    setcompletion = len(primerset)/len(totalset)
+    return {'setcompletion':setcompletion,'total_score':int(round((setcompletion* 1000),0))}
+    
+
+sfile = myargs.r
+kfile = myargs.k
+os.chdir(os.getcwd())
+cores = os.cpu_count()
+
+def make_splits(x, num_splits):
+    splits = []
+    position = 0
+    for r in range(num_splits):
+        if r!= num_splits-1:
+            splits.append(x[position:position+len(x)//num_splits])
+        else:
+            splits.append(x[position:])
+        position+=len(x)//num_splits
+    return splits
+
+print("Loading references and motifs. {}".format(time.asctime()))
+myseqs = list(SeqIO.parse(sfile,'fasta',IUPAC.unambiguous_dna))
+
+totalset = set([r.id for r in myseqs])
+
+kmers = list(SeqIO.parse(kfile,'fasta',IUPAC.ambiguous_dna))
+
+alignments = {}
+
+def find_alignments(kmer_list):
+    my_align = {}
+    for k in kmer_list:
+        my_align[str(k.seq)] = {r.id:nt_search(str(r.seq),str(k.seq))[1:] for r in myseqs}
+    return my_align
+    
+def multi_map(func, data):
+    with Pool(cores) as pool:
+        kmer_splits = make_splits(kmers,cores)
+        results = pool.map(func, kmer_splits)
+        return results
+
+print("Mapping motifs with {} cores. {}".format(cores,time.asctime()))
+mapped_kmers = multi_map(find_alignments,kmers)
+
+for m in mapped_kmers:
+    alignments.update(m)
+
+#there may be a more effecient way to do this ¯\_(ツ)_/¯
+del(mapped_kmers)
+
+print("Building score table and bed files. {}".format(time.asctime()))
+score_table = {}
+bed = []
+for primer in alignments.keys():
+    score = pscore(primer,set([k for k in alignments[primer].keys() if alignments[primer][k]]))
+    score_table[primer] = score
+    for records in alignments[primer].items():
+        if records[1]:
+            for pos in records[1]:
+                bed.append({'chr':records[0],'start':pos,'end':pos+len(primer),'name':primer,'score':score['total_score']})
+
+print(len(bed))
+score_df = pd.DataFrame(score_table).T
+bed_df = pd.DataFrame(bed)
+bed_df = bed_df[['chr','start','end','name','score']]
+beds = bed_df.groupby('chr')
+print("Writing bed files. {}".format(time.asctime()))
+for accession in beds.groups.keys():
+    beds.get_group(accession).to_csv(accession.replace('|','_')+'_primers.bed',sep='\t',header=False,index=False)
+score_df.to_csv(kfile+"_score_table.csv")
+# for seq in myseqs:
+#     alignments[seq.id] = [nt_search(str(seq.seq),str(k.seq)) for k in kmers]
