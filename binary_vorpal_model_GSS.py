c56dad189f0844e4796dc40e186053954f0841a1
Phil Davis <pdavis@mriglobal.org>
2021-04-06 19:52:04 -0400

    fixed bugs in GSS script

--- binary_vorpal_model_GSS.py
+++ binary_vorpal_model_GSS.py
@@ -63,18 +63,20 @@ print("Assigning variables.")
 X = features.values
 y = labels
 
-clf = LogisticRegressionCV(Cs=10,penalty='l1',verbose=3,solver='liblinear',cv=gss,max_iter=iterations,n_jobs=-1,tol=tolerance)
+parameters = {'C':[.00001,.0001,.001,.01,.1,1,10,100,1000,10000]} #Cs:10
+logit = LogisticRegression(penalty='l1',verbose=1,solver='liblinear',max_iter=iterations,n_jobs=-1,tol=tolerance)
+clf = GridSearchCV(logit,parameters,scoring='accuracy',cv=gss,n_jobs=-1,return_train_score=False)
 print("Fitting model.")
 clf.fit(X,y,groups=groups)
 print("Training complete.")
 print("Trained model score (accuracy):",clf.score(X,y))
 
-#cv_results = pd.DataFrame(clf.cv_results_).sort_values('mean_test_score')
-#print("Max mean CV score: {}".format(cv_results['mean_test_score'].max)
-#print("CV results:")
-#print(cv_results)
+cv_results = pd.DataFrame(clf.cv_results_).sort_values('mean_test_score')
+print("Max mean CV score: {}".format(cv_results['mean_test_score'].max()))
+print("CV results:")
+print(cv_results)
 
-model_coef = pd.Series(dict(zip(features.columns[(clf.coef_ !=0)[0]],clf.coef_[(clf.coef_ != 0)])))
+model_coef = pd.Series(dict(zip(features.columns[(clf.best_estimator_.coef_ !=0)[0]],clf.best_estimator_.coef_[(clf.best_estimator_.coef_ != 0)])))
 
 print("Model predictors with coefficients greater than 0:")
 print(model_coef)
@@ -84,6 +86,6 @@ os.chdir(cwd)
 features.columns.values.dump(metafile.split('.')[0]+"_feature_array.pickle")
 #output sparse model coefficients
 model_coef.to_csv(metafile.split('.')[0]+"_model_coefficients.tsv",sep='\t')
-#cv_results.to_csv(metafile.split('.')[0]+"_cv_results.tsv",sep='\t')
+cv_results.to_csv(metafile.split('.')[0]+"_cv_results.tsv",sep='\t')
 #output serialized classifier object for persistence
 joblib.dump(clf,metafile.split('.')[0]+"_CLF.joblib")
